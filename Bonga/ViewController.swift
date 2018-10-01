e186856e21232707a7b71e4a7c7b0618d6eebd7c
rony quail <ronymarinequail@hotmail.com>
2018-10-01 17:47:55 +0300

    Initial Commit

---
+++ Bonga/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bonga
+//
+//  Created by RastaOnAMission on 01/10/2018.
+//  Copyright © 2018 ronyquail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
